46ac28892dde19f2b063c49063d497dc0027f2cd
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-05-06 00:42:40 +0100

    API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  const apiUrl = 'http://' + window.location.hostname + ':5001/api/v1/status/';
+
+  // Request API status
+  $.get(apiUrl, function (response) {
+    if (response.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+
+  const amenities = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    if (Object.values(amenities).length === 0) {
+      $('.amenities h4').html('&nbsp;');
+    } else {
+      $('.amenities h4').text(Object.values(amenities).join(', '));
+    }
+  });
+});
