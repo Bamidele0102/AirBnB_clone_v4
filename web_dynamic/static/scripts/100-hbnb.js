e5ab6ae0c1e93714841eb48dcaab68e6b77b62cd
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-05-06 02:05:23 +0100

    States and Cities

---
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -0,0 +1,126 @@
+$('document').ready(function () {
+    const api = 'http://' + window.location.hostname;
+  
+    // Check API status and update UI
+    $.get(api + ':5001:/api/v1/status/', function (response) {
+      if (response.status === 'OK') {
+        $('DIV#api_status').addClass('available');
+      } else {
+        $('DIV#api_status').removeClass('available');
+      }
+    });
+  
+    // Fetch initial places data and append to DOM
+    $.ajax({
+      url: api + ':5001/api/v1/places_search/',
+      type: 'POST',
+      data: '{}',
+      contentType: 'application/json',
+      dataType: 'json',
+      success: appendPlaces
+    });
+  
+    let states = {};
+    // Listen to changes on state checkboxes
+    $('.locations > UL > H2 > INPUT[type="checkbox"]').change(function () {
+      if ($(this).is(':checked')) {
+        states[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete states[$(this).attr('data-id')];
+      }
+      updateLocations();
+    });
+  
+    let cities = {};
+    // Listen to changes on city checkboxes
+    $('.locations > UL > UL > LI INPUT[type="checkbox"]').change(function () {
+      if ($(this).is(':checked')) {
+        cities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete cities[$(this).attr('data-id')];
+      }
+      updateLocations();
+    });
+  
+    let amenities = {};
+    // Listen to changes on amenity checkboxes
+    $('.amenities INPUT[type="checkbox"]').change(function () {
+      if ($(this).is(':checked')) {
+        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete amenities[$(this).attr('data-id')];
+      }
+      updateAmenities();
+    });
+  
+    // Listen to button click for searching places
+    $('BUTTON').click(function () {
+      $.ajax({
+        url: api + ':5001/api/v1/places_search/',
+        type: 'POST',
+        data: JSON.stringify({
+          'states': Object.keys(states),
+          'cities': Object.keys(cities),
+          'amenities': Object.keys(amenities)
+        }),
+        contentType: 'application/json',
+        dataType: 'json',
+        success: appendPlaces
+      });
+    });
+  
+    // Function to append places to DOM
+    function appendPlaces(data) {
+      $('SECTION.places').empty();
+      $('SECTION.places').append(data.map(place => {
+        return `<ARTICLE>
+                  <DIV class="title">
+                    <H2>${place.name}</H2>
+                    <DIV class="price_by_night">
+                      ${place.price_by_night}
+                    </DIV>
+                  </DIV>
+                  <DIV class="information">
+                    <DIV class="max_guest">
+                      <I class="fa fa-users fa-3x" aria-hidden="true"></I>
+                      </BR>
+                      ${place.max_guest} Guests
+                    </DIV>
+                    <DIV class="number_rooms">
+                      <I class="fa fa-bed fa-3x" aria-hidden="true"></I>
+                      </BR>
+                      ${place.number_rooms} Bedrooms
+                    </DIV>
+                    <DIV class="number_bathrooms">
+                      <I class="fa fa-bath fa-3x" aria-hidden="true"></I>
+                      </BR>
+                      ${place.number_bathrooms} Bathrooms
+                    </DIV>
+                  </DIV>
+                  <DIV class="description">
+                    ${place.description}
+                  </DIV>
+                </ARTICLE>`;
+      }));
+    }
+  
+    // Function to update the locations display
+    function updateLocations() {
+      const locations = Object.assign({}, states, cities);
+      if (Object.values(locations).length === 0) {
+        $('.locations H4').html('&nbsp;');
+      } else {
+        $('.locations H4').text(Object.values(locations).join(', '));
+      }
+    }
+  
+    // Function to update the amenities display
+    function updateAmenities() {
+      if (Object.values(amenities).length === 0) {
+        $('.amenities H4').html('&nbsp;');
+      } else {
+        $('.amenities H4').text(Object.values(amenities).join(', '));
+      }
+    }
+  });
+  
