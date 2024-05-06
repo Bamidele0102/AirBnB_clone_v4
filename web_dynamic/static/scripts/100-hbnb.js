b8d1217c92f5ff85c7427f3ec3716824048b93a0
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-05-06 02:24:51 +0100

    Reviews

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,79 +1,79 @@
 $('document').ready(function () {
-    const api = 'http://' + window.location.hostname;
-  
-    // Check API status and update UI
-    $.get(api + ':5001:/api/v1/status/', function (response) {
-      if (response.status === 'OK') {
-        $('DIV#api_status').addClass('available');
-      } else {
-        $('DIV#api_status').removeClass('available');
-      }
-    });
-  
-    // Fetch initial places data and append to DOM
+  const api = 'http://' + window.location.hostname;
+
+  // Check API status and update UI
+  $.get(api + ':5001:/api/v1/status/', function (response) {
+    if (response.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+  // Fetch initial places data and append to DOM
+  $.ajax({
+    url: api + ':5001/api/v1/places_search/',
+    type: 'POST',
+    data: '{}',
+    contentType: 'application/json',
+    dataType: 'json',
+    success: appendPlaces
+  });
+
+  const states = {};
+  // Listen to changes on state checkboxes
+  $('.locations > UL > H2 > INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      states[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete states[$(this).attr('data-id')];
+    }
+    updateLocations();
+  });
+
+  const cities = {};
+  // Listen to changes on city checkboxes
+  $('.locations > UL > UL > LI INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      cities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete cities[$(this).attr('data-id')];
+    }
+    updateLocations();
+  });
+
+  const amenities = {};
+  // Listen to changes on amenity checkboxes
+  $('.amenities INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    updateAmenities();
+  });
+
+  // Listen to button click for searching places
+  $('BUTTON').click(function () {
     $.ajax({
       url: api + ':5001/api/v1/places_search/',
       type: 'POST',
-      data: '{}',
+      data: JSON.stringify({
+        states: Object.keys(states),
+        cities: Object.keys(cities),
+        amenities: Object.keys(amenities)
+      }),
       contentType: 'application/json',
       dataType: 'json',
       success: appendPlaces
     });
-  
-    let states = {};
-    // Listen to changes on state checkboxes
-    $('.locations > UL > H2 > INPUT[type="checkbox"]').change(function () {
-      if ($(this).is(':checked')) {
-        states[$(this).attr('data-id')] = $(this).attr('data-name');
-      } else {
-        delete states[$(this).attr('data-id')];
-      }
-      updateLocations();
-    });
-  
-    let cities = {};
-    // Listen to changes on city checkboxes
-    $('.locations > UL > UL > LI INPUT[type="checkbox"]').change(function () {
-      if ($(this).is(':checked')) {
-        cities[$(this).attr('data-id')] = $(this).attr('data-name');
-      } else {
-        delete cities[$(this).attr('data-id')];
-      }
-      updateLocations();
-    });
-  
-    let amenities = {};
-    // Listen to changes on amenity checkboxes
-    $('.amenities INPUT[type="checkbox"]').change(function () {
-      if ($(this).is(':checked')) {
-        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-      } else {
-        delete amenities[$(this).attr('data-id')];
-      }
-      updateAmenities();
-    });
-  
-    // Listen to button click for searching places
-    $('BUTTON').click(function () {
-      $.ajax({
-        url: api + ':5001/api/v1/places_search/',
-        type: 'POST',
-        data: JSON.stringify({
-          'states': Object.keys(states),
-          'cities': Object.keys(cities),
-          'amenities': Object.keys(amenities)
-        }),
-        contentType: 'application/json',
-        dataType: 'json',
-        success: appendPlaces
-      });
-    });
-  
-    // Function to append places to DOM
-    function appendPlaces(data) {
-      $('SECTION.places').empty();
-      $('SECTION.places').append(data.map(place => {
-        return `<ARTICLE>
+  });
+
+  // Function to append places to DOM
+  function appendPlaces (data) {
+    $('SECTION.places').empty();
+    $('SECTION.places').append(data.map(place => {
+      return `<ARTICLE>
                   <DIV class="title">
                     <H2>${place.name}</H2>
                     <DIV class="price_by_night">
@@ -101,26 +101,25 @@ $('document').ready(function () {
                     ${place.description}
                   </DIV>
                 </ARTICLE>`;
-      }));
+    }));
+  }
+
+  // Function to update the locations display
+  function updateLocations () {
+    const locations = Object.assign({}, states, cities);
+    if (Object.values(locations).length === 0) {
+      $('.locations H4').html('&nbsp;');
+    } else {
+      $('.locations H4').text(Object.values(locations).join(', '));
     }
-  
-    // Function to update the locations display
-    function updateLocations() {
-      const locations = Object.assign({}, states, cities);
-      if (Object.values(locations).length === 0) {
-        $('.locations H4').html('&nbsp;');
-      } else {
-        $('.locations H4').text(Object.values(locations).join(', '));
-      }
+  }
+
+  // Function to update the amenities display
+  function updateAmenities () {
+    if (Object.values(amenities).length === 0) {
+      $('.amenities H4').html('&nbsp;');
+    } else {
+      $('.amenities H4').text(Object.values(amenities).join(', '));
     }
-  
-    // Function to update the amenities display
-    function updateAmenities() {
-      if (Object.values(amenities).length === 0) {
-        $('.amenities H4').html('&nbsp;');
-      } else {
-        $('.amenities H4').text(Object.values(amenities).join(', '));
-      }
-    }
-  });
-  
+  }
+});
--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,30 +1,30 @@
 $('document').ready(function () {
-    const api = 'http://' + window.location.hostname;
-  
-    $.get(api + ':5001/api/v1/status/', function (response) {
-      if (response.status === 'OK') {
-        $('DIV#api_status').addClass('available');
-      } else {
-        $('DIV#api_status').removeClass('available');
-      }
+  const api = 'http://' + window.location.hostname;
+
+  $.get(api + ':5001/api/v1/status/', function (response) {
+    if (response.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+  $('#search_button').click(function () {
+    const amenities = [];
+    $('INPUT[type="checkbox"]:checked').each(function () {
+      amenities.push($(this).attr('data-id'));
     });
-  
-    $('#search_button').click(function () {
-      const amenities = [];
-      $('INPUT[type="checkbox"]:checked').each(function () {
-        amenities.push($(this).attr('data-id'));
-      });
-  
-      $.ajax({
-        url: api + ':5001/api/v1/places_search/',
-        type: 'POST',
-        contentType: 'application/json',
-        dataType: 'json',
-        data: JSON.stringify({ amenities: amenities }),
-        success: function (data) {
-          $('SECTION.places').empty();
-          data.forEach(place => {
-            $('SECTION.places').append(`<article>
+
+    $.ajax({
+      url: api + ':5001/api/v1/places_search/',
+      type: 'POST',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({ amenities }),
+      success: function (data) {
+        $('SECTION.places').empty();
+        data.forEach(place => {
+          $('SECTION.places').append(`<article>
               <div class="title">
                 <h2>${place.name}</h2>
                 <div class="price_by_night">${place.price_by_night}</div>
@@ -47,9 +47,8 @@ $('document').ready(function () {
                 ${place.description}
               </div>
             </article>`);
-          });
-        }
-      });
+        });
+      }
     });
   });
-  
+});
